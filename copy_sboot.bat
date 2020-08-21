1b240eb88c60751f6e72e796dfbbf889cba1c8b2
huang yang <elious.huang@gmail.com>
2020-08-21 23:41:02 +0800

    chg: change sboot copy folder

--- copy_sboot.bat
+++ copy_sboot.bat
@@ -1,7 +1,7 @@
 
 @echo off
 SET LIB_FILE=.\objects\sboot.bin
-SET DEST_FOLDER=..\rivet_gun\control\control\
+SET DEST_FOLDER=..\rivet_gun\control\tool\
 
 xcopy /e /q /i /y %LIB_FILE%    %DEST_FOLDER%
 
